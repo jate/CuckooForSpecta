9916c146e27354b87b81d7c386dcef97e6da977c
rbaumbach <rbaumbach.github@gmail.com>
2015-05-12 07:25:45 -0700

    Add Specta/Expecta/OCMock with Cocoapods

--- Specs/Specs.m
+++ Specs/Specs.m
@@ -1,32 +1,17 @@
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import <Specta/Specta.h>
+#import <Expecta/Expecta.h>
+#import <OCMock/OCMock.h>
 
-@interface Specs : XCTestCase
+SpecBegin(Thing)
 
-@end
+describe(@"Thing", ^{
+    it(@"s yes", ^{
+        expect(YES).to.beTruthy();
+    });
+    
+    it(@"s no", ^{
+        expect(NO).to.beTruthy();
+    });
+});
 
-@implementation Specs
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SpecEnd
